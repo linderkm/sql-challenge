281875c50f39ffc7b60a4404789f611caaa127ec
linderkm <kevin.linder94@gmail.com>
2024-11-18 20:49:43 -0700

    adding new query (analysis q3)

--- employeeSQL/analysis.sql
+++ employeeSQL/analysis.sql
@@ -10,5 +10,13 @@ FROM employees
 WHERE hire_date = ''
 
 
+-- 3. List the manager of each department along with:
+-- their department number, department name, employee number, last name, and first name
+
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.first_name, employees.last_name
+FROM dept_manager
+JOIN departments ON departments.dept_no = dept_manager.dept_no
+JOIN employees ON employees.emp_no = dept_manager.emp_no;
+
 
 
