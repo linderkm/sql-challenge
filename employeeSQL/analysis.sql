2b7f54c75ab6f7add1be8ffc501f62bc4f1fb2f1
linderkm <kevin.linder94@gmail.com>
2024-11-18 21:13:54 -0700

    adding two additional queries (q4 and q5)

--- employeeSQL/analysis.sql
+++ employeeSQL/analysis.sql
@@ -5,6 +5,7 @@ JOIN salaries ON salaries.emp_no=employees.emp_no;
 
 
 -- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
+--https://stackoverflow.com/questions/207190/sql-server-string-to-date-conversion
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date = ''
@@ -12,11 +13,24 @@ WHERE hire_date = ''
 
 -- 3. List the manager of each department along with:
 -- their department number, department name, employee number, last name, and first name
-
-SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.first_name, employees.last_name
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.first_name,employees.last_name
 FROM dept_manager
 JOIN departments ON departments.dept_no = dept_manager.dept_no
 JOIN employees ON employees.emp_no = dept_manager.emp_no;
 
 
 
+--4. List the department number for each employee along with:
+--that employee’s employee number, last name, first name, and department name
+SELECT de.dept_no, de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+JOIN employees e ON e.emp_no = de.emp_no
+JOIN departments d ON d.dept_no = de.dept_no;
+
+
+-- 5. List first name, last name, and sex of each employee whose first name is
+-- Hercules and whose last name begins with the letter B.
+-- https://stackoverflow.com/questions/26411705/selecting-rows-which-contain-certain-characters
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B__%';
